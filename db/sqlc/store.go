29b3c9f7a189321e225419c9c5d09604f07d7954
arya2004 <arya20j4@gmail.com>
2024-07-28 16:51:49 +0530

    chore: Refactor store.go to use transaction for TransferTx method

---
+++ db/sqlc/store.go
@@ -0,0 +1,116 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// run query individyally as combined
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+		Queries: New(db),
+	}
+}
+
+
+
+func (store *Store) execTx (ctx context.Context, fn func(*Queries) error  ) error{
+	tx, err := store.db.BeginTx(ctx, nil)
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountId int64 `json:"from_account_id"`
+	ToAccountId   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+var txKey = struct{}{}
+
+// create transfer record, add account entryes, uipdate acc balance
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error{
+
+		var err error
+
+		txName := ctx.Value(txKey)
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID:   arg.ToAccountId,
+			Amount:        arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+
+		fmt.Println(txName, "create Entry 1")
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountId,
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "create Entry 2")
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountId,
+			Amount:    arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "Get Account 1 for update")
+
+		//Todo: update Account Balance and avoiding Deadlock
+
+		//get acc from db -> change balance
+
+
+		return nil
+	})
+
+	return result, err
+}
+	
